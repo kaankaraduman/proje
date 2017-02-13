cacf84dc7767d3a047dc2939a895aec4a1bfe19b
Kaan Karaduman <kaankaraduman19@gmail.com>
2017-02-13 12:21:20 +0200

    commit9

--- commit9.py
+++ commit9.py
@@ -1,3 +1,6 @@
 def sinav():
     sinav_not = int(input("Öğrencinin notunu giriniz:"))
     print(sinav_not)
+    sınav_not2 = int(input("Öğrencinin notunu giriniz:"))
+    ort = (sinav_not + sinav_not2) / 2
+    print(ort)
